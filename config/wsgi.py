f917bd4792c86aa1a64e784d1c3a405d47480786
Islidexz <adk_6@mail.ru>
2024-04-24 00:25:18 +0300

    modified:   .gitignore 	modified:   config/wsgi.py 	modified:   manage.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -4,3 +4,4 @@ from django.core.wsgi import get_wsgi_application
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.dev')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.prod')
 application = get_wsgi_application()
+print(os.environ['DJANGO_SETTINGS_MODULE'])
