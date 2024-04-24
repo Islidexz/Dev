0d3ac69befa8cfba4d95fe4127690d563538e53b
Islidexz <adk_6@mail.ru>
2024-04-24 07:06:41 +0300

    modified:   config/environment_settings.py

--- config/environment_settings.py
+++ config/environment_settings.py
@@ -13,3 +13,33 @@ def set_django_settings_module():
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.dev')
 
 #set_django_settings_module()
+
+import os
+
+def set_django_settings_module():
+    # Специфичная часть пути для сервера
+    server_path_segment = 'www/to-create.online/nails'
+    # Получаем имя текущей ветки Git
+    branch_name = os.popen('git rev-parse --abbrev-ref HEAD').read().strip()
+    # Получаем абсолютный путь к директории, где находится manage.py
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    print(f"Текущая ветка: {branch_name}")
+    print(f"BASE_DIR: {base_dir}")
+
+    # Проверяем имя ветки и путь
+    if branch_name == 'production':
+        print("------PRODUCTION: Устанавливаем настройки для PRODUCTION--------")
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.prod')
+    elif branch_name == 'main' and server_path_segment in base_dir.replace('\\', '/'):
+        print("------PRODUCTION: Устанавливаем настройки для PRODUCTION по пути--------")
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.prod')
+    else:
+        print("--------LOCAL: Устанавливаем настройки для LOCAL----------")
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.dev')
+
+# Вызываем функцию в manage.py
+if __name__ == '__main__':
+    set_django_settings_module()
+
+    # Остальной код manage.py...
